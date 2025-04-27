cb2cd8d9c46a8759bddfab1791eb69a326da5984
maja <maya-kut@mail.ru>
2025-04-27 16:34:50 +0300

    find JUnit5 - done

--- src/test/java/WikiSearch.java
+++ src/test/java/WikiSearch.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -21,12 +22,24 @@ public class WikiSearch {
 
         @Test
         void findSelenideWikiSearch() {
+            //переход на страницу гитхаба
             open("https://github.com/");
             $("[data-action='click:qbsearch-input#searchInputContainerClicked']").click();
+
+            //ввод значения для поиска
             $("#query-builder-test").setValue("selenide").pressEnter();
             $$("[data-testid='results-list']").first().$("a").click();
+
+            //переход во вкладку Wiki
             $("#wiki-tab").click();
+
+            //проверка текста Soft assertions
             $("#wiki-body").shouldHave(text("Soft assertions"));
+            $$("a").findBy(text("Soft assertions")).click();
+
+            //проверка наличия примера о JUnit5
+            $("#repo-content-pjax-container").shouldHave(text("JUnit5"));
+
 
 
             sleep(10000);
