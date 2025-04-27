e4ca0f56e19c625acd241b33091c7260de5eddc4
maja <maya-kut@mail.ru>
2025-04-27 19:55:42 +0300

    small refactoring

--- src/test/java/EnterprizeSearch.java
+++ src/test/java/EnterprizeSearch.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.CollectionCondition.exactTexts;
 import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
@@ -25,8 +26,10 @@ public class EnterprizeSearch {
             void findSelenideEnterprize() {
                 //переход на страницу гитхаба
                 open("https://github.com/");
-                $(".HeaderMenu-nav").find(byText("Solutions")).click();
-                $$("a").findBy(text("Enterprises")).click();
+//                $(".HeaderMenu-nav").find(byText("Solutions")).click();
+                $(byTagAndText("button","Solutions")).hover();
+//                $$("a").findBy(text("Enterprises")).click();
+                $(byTagAndText("a","Enterprises")).click();
 
                 $("#hero-section-brand-heading").shouldHave(text("The AI-powered"));
                 $("#hero-section-brand-heading").shouldHave(text("developer platform"));
